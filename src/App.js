23ea0192bc47b791b40dedf4ae72329029d09640
Andrej16 <31798010+Andrej16@users.noreply.github.com>
2022-07-06 16:27:18 +0300

    Added button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import './App.css';
 import { StyledButton } from './styled-components/StyledButton';
 import {SupportForm} from './SupportForm/SupportForm';
 import { NiceControl } from './Stilization controls/NiceControl';
+import { Icon } from './components/Icon';
+import { IconTypes } from './constants/icons';
+import { Button } from './components/Button';
 
 function App() {
   return (
@@ -14,8 +17,12 @@ function App() {
       <FormToJson></FormToJson>
       <StyledButton/>
       <FormSyncControl/>
-      <SupportForm/>*/}
+      <SupportForm/>
       <NiceControl></NiceControl>
+      <Icon type={IconTypes.cross} color="red" id="22" className="className"></Icon>
+      <Icon type={IconTypes.plus} size="16px" id="23"></Icon>*/}
+      <Button className="customClass" id="id" iconType={IconTypes.cross}>First</Button>
+      <Button className="customClass" size="small" color="primary" id="id" iconType={IconTypes.plus}>Second</Button>
     </div>
   );
 }
